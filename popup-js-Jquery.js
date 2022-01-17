e63eef89cdbec2ceb1694ecda887feadf6901edd
Anmol Singh <anmolhive@gmail.com>
2022-01-17 20:56:25 +0530

    Add files via upload
    This Is The Simple Jquery Code. 
    Which Simply Opens A Popup, 
    Close By Clicking On Cross Icon And, 
    Also, Close It When We click Outside the Popup.
    In This Repository, There Is No Extra Code Not Even HTML Popup OR Any Thing.

---
+++ popup-js-Jquery.js
@@ -0,0 +1,24 @@
+//Popup-JS-Jquery
+
+$(".eventwrap-list h6").click(function(e){  //Get Target by Click Event.
+    e.preventDefault();
+    $(".eventpopup").fadeIn(20,"linear");   //Opening Popeup. 
+});
+
+$('.crossicon').click(function() {          //Get Target Cross Icon by Click Event.
+   $(".eventpopup").fadeOut("fast");        //Close Popup.
+   $( ".eventwrap-list h6" ).removeClass( "evnetpopupfind" ); //Removing A Class.
+});
+
+$(document).mouseup(function(e)            //Start MouseUp Event 
+{
+    var container = $(".eventpopup");      //Target Popup Div.
+    
+    // If the target of the click isn't the container nor a descendant of the container
+    if (!container.is(e.target) && container.has(e.target).length === 0) 
+    {   // If The Click Takes Place Outside The Container That Is Popup Div.  
+        container.fadeOut("fast");         //Close The Popup.
+        $( ".eventwrap-list h6" ).removeClass( "evnetpopupfind" );
+        
+    }
+});
